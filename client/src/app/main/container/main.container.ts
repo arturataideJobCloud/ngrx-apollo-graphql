38153dfca4a5e3eb05910aa5bb08ea00156d0603
Artur Ataide <artur.ataide@jobcloud.ch>
2020-04-23 23:24:02 +0200

    add background

--- client/src/app/main/container/main.container.ts
+++ client/src/app/main/container/main.container.ts
@@ -3,7 +3,13 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
 @Component({
   selector: 'app-main',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  template: `Test`,
-  styles: [''],
+  template: `<img src="assets/images/pitch.jpg" class="pitch" />`,
+  styles: [
+    `
+      img.pitch {
+        width: 90vw;
+      }
+    `,
+  ],
 })
 export class MainComponent {}
